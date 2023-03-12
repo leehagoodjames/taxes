5b4e6228c0f58d64dfa9bdefff1a307e2cbd8f33
Lee James <40045512+leehagoodjames@users.noreply.github.com>
2023-03-11 20:05:35 -0500

    Add example

---
+++ examples/example.py
@@ -0,0 +1,25 @@
+# Local Imports
+from brackets import FederalIncomeTaxBrackets
+from brackets import GeorgiaStateIncomeTaxBrackets
+from brackets import SocialSecurityIncomeTaxBrackets
+from brackets import MedicareIncomeTaxBrackets
+
+# Load in tax brackets for your year and filing-status.
+federal_taxes = FederalIncomeTaxBrackets.married_filing_jointly_2022_tax
+georgia_taxes = GeorgiaStateIncomeTaxBrackets.married_filing_jointly_2022_tax
+social_security_taxes = SocialSecurityIncomeTaxBrackets.social_security_employee_2022_tax
+medicare_taxes = MedicareIncomeTaxBrackets.medicare_employee_2022_tax
+
+# Compute the taxes.
+agi = 100000
+federal_tax = federal_taxes.calculate_taxes(agi)
+state_tax = georgia_taxes.calculate_taxes(agi)
+social_security_tax = social_security_taxes.calculate_taxes(agi)
+medicare_tax = medicare_taxes.calculate_taxes(agi)
+
+# Show the result.
+print(f'adjusted gross income: ${agi:,.0f}')
+print(f'federal tax: ${federal_tax:,.0f}')
+print(f'state tax: ${state_tax:,.0f}')
+print(f'social security tax: ${social_security_tax:,.0f}')
+print(f'medicare tax: ${medicare_tax:,.0f}')
